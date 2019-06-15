abfcd64be83f3e02bccd8086e8bddb7eaca3b3e7
daniel-koehler <daniel.koehler95@googlemail.com>
2019-06-15 19:44:31 +0200

    Put spike_t and corresponding utils in its own module.

---
+++ src/spike.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "spike.h"
+
+
+int compare_spikes(const void *p1, const void *p2){
+    /*
+    Compare function for qsort, that determines the interspike interval between the given spikes
+    */
+    spike_t *spike1 = *(spike_t **) p1;
+    spike_t *spike2 = *(spike_t **) p2;
+
+    if(spike1->t < spike2->t) return -1;
+    if(spike1->t > spike2->t) return 1;
+    else return 0;
+}
+
+spike_t *sort_spikes(spike_t *head, int spike_cnt){
+    /* 
+    Sorts spikes in a linked list based on their time t returns the new head of the list.
+    */
+    spike_t *curr_spike = head;
+    spike_t **spike_array = (spike_t **) malloc(sizeof(spike_t *)*spike_cnt);
+    int i = 0;
+    if(spike_cnt > 0){
+        while(curr_spike){
+            spike_array[i++] = curr_spike;
+            curr_spike = curr_spike->next;
+        }
+        qsort(spike_array, spike_cnt, sizeof(spike_t *), compare_spikes);
+        for(i = 0; i < spike_cnt - 1; i++){
+            spike_array[i]->next = spike_array[i+1];
+        }
+        spike_array[spike_cnt - 1]->next = NULL;
+        curr_spike = spike_array[0];
+        free(spike_array);
+        return curr_spike;
+    }
+    else return NULL;
+}
+
+void print_spikes(spike_t *head){
+    spike_t *curr_spike = head;
+    while(curr_spike){
+        printf("Neuron %i at %f ms\n", curr_spike->index, curr_spike->t);
+        curr_spike = curr_spike->next;
+    }
+}
+
+spike_t *append_spike(spike_t *spike, float t, int idx){
+    /*
+    Appends a spike to node 'spike' and returns pointer to it.
+    */
+    spike->next = (spike_t *) malloc(sizeof(spike_t));
+    spike = spike->next;
+    spike->next   = NULL;
+    spike->index  = idx;
+    spike->t      = t;
+    return spike;
+}
+
+void free_spikes(spike_t *top_input){
+    spike_t *tmp;
+    while(top_input){
+        tmp = top_input;
+        top_input = top_input->next;
+        free(tmp);
+    }
+}
---
+++ src/spike.h
@@ -0,0 +1,16 @@
+#ifndef spike_h
+#define spike_h
+
+typedef struct spike_t {
+    float   t;
+    int     index;
+    struct spike_t *next;
+} spike_t;
+
+int compare_spikes(const void *p1, const void *p2);
+spike_t *sort_spikes(spike_t *head, int spike_cnt);
+void print_spikes(spike_t *head);
+spike_t *append_spike(spike_t *spike, float t, int idx);
+void free_spikes(spike_t *top_input);
+
+#endif
